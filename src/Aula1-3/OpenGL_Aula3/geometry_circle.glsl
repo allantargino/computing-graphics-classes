25694dc0702233e21bb05b6156c755b950c0ac7d
Allan Targino <altargin@microsoft.com>
2017-11-29 13:24:54 -0200

    optimization

--- src/Aula1-3/OpenGL_Aula3/geometry_circle.glsl
+++ src/Aula1-3/OpenGL_Aula3/geometry_circle.glsl
@@ -2,41 +2,33 @@
 
 layout (points) in;
 layout (line_strip, max_vertices = 101) out;
-//layout (points) out;
 
 in vec3 vs_color[];
+
 out vec3 fs_color;
 
+const float pi = 3.1415926f;
+const float divisions = 100.0f;
 
 void main()
 {
-    int i;
-    for(i = 0;i < gl_in.length();i++)
+    for(int i = 0; i < gl_in.length(); i++)
     {
         fs_color = vs_color[i];
-        //gl_PointSize = 20.0;
-        gl_Position = gl_in[i].gl_Position;
-        //EmitVertex();
+        gl_PointSize = 3.0f;
 
-        float pi = 3.1415926;
+        float radius = 0.3f;
+
         float pi_2 = pi * 2;
-
-        float radius = 0.3;
-        float divisions = 100;
         float step = pi_2 / divisions;
+        float complete_circle = pi_2+step;
 
-        float x = gl_Position.x;
-        float y = gl_Position.y;
-        float z = gl_Position.z;
-
-        for(float j = 0; j <= pi_2+step; j+=step)  //generate vertices at positions on the circumference from 0 to 2*pi
+        for(float j = 0; j <= complete_circle; j+=step)
         {
-            gl_PointSize = 3.0;
-            gl_Position = gl_in[i].gl_Position + vec4(x+ radius*cos(j), y + radius*sin(j), z, 1.0);   //circle parametric equation
+            gl_Position = gl_in[i].gl_Position + vec4(gl_in[i].gl_Position.x+ radius*cos(j), gl_in[i].gl_Position.y + radius*sin(j), gl_in[i].gl_Position.z, 1.0);
             EmitVertex();
         }
 
         EndPrimitive();
     }
-    //EndPrimitive();
 }
