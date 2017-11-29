16405325548e042452846528b93b858b0deb5191
Allan Targino <altargin@microsoft.com>
2017-11-29 13:18:29 -0200

    perfect circles with geometry shader

--- src/Aula1-3/OpenGL_Aula3/geometry_circle.glsl
+++ src/Aula1-3/OpenGL_Aula3/geometry_circle.glsl
@@ -1,12 +1,12 @@
 #version 400
 
 layout (points) in;
-layout (line_strip) out;
+layout (line_strip, max_vertices = 101) out;
 //layout (points) out;
-layout (max_vertices = 100) out;
 
 in vec3 vs_color[];
 out vec3 fs_color;
+
 
 void main()
 {
@@ -18,18 +18,21 @@ void main()
         gl_Position = gl_in[i].gl_Position;
         //EmitVertex();
 
-        float radius = 0.5;
-        //float divisions = 5;
-        //float angle = 360 / divisions;
+        float pi = 3.1415926;
+        float pi_2 = pi * 2;
+
+        float radius = 0.3;
+        float divisions = 100;
+        float step = pi_2 / divisions;
 
         float x = gl_Position.x;
         float y = gl_Position.y;
         float z = gl_Position.z;
 
-        for(float j = 0; j <= 6.28 ; j+=0.628)  //generate vertices at positions on the circumference from 0 to 2*pi
+        for(float j = 0; j <= pi_2+step; j+=step)  //generate vertices at positions on the circumference from 0 to 2*pi
         {
             gl_PointSize = 3.0;
-            gl_Position = gl_Position + vec4(x+ radius*cos(j), y + radius*sin(j), z, 1.0);   //circle parametric equation
+            gl_Position = gl_in[i].gl_Position + vec4(x+ radius*cos(j), y + radius*sin(j), z, 1.0);   //circle parametric equation
             EmitVertex();
         }
 
